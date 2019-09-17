168229c226691330ea98ad9b3750c45162f486a1
Uzayisenga <aminauzayisenga@gmail.com>
2019-09-17 14:11:08 +0200

    After testing how the app works in the step 8

--- Gemfile
+++ Gemfile
@@ -38,6 +38,11 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'factory_bot_rails'
+  gem 'rspec-rails'
+  gem 'spring-commands-rspec'
 end
 
 group :development do
---
+++ spec/features/task.spec.rb
@@ -0,0 +1,48 @@
+# In this require, the feature required for Feature Spec such as Capybara are available.
+require 'rails_helper'
+
+# On the right side of this RSpec.feature, write the test item name like "task management feature" (grouped by do ~ end)
+RSpec.feature "Task management function", type: :feature do
+  # In scenario (alias of it), write the processing of the test for each item you want to check.
+ scenario "Test task list" do
+    # Create two tasks in advance to use in the task list test
+    Task.create!(name: 'test_task_01', content: 'testtesttest')
+    Task.create!(name: 'test_task_02', content: 'samplesample')
+
+  # visit to tasks_path (transition to task list page)
+    visit tasks_path
+
+  # write a test to verify that the string "" testtesttest "" samplesample "is included when accessing the task list page using have_content method
+    
+    expect(page).to have_content 'testtesttest'
+    expect(page).to have_content 'samplesample' 
+    save_and_open_page
+
+  end
+
+  scenario "Test task creation" do
+    visit new_task_path
+    fill_in "Name", with: "Names"
+    fill_in "Content", with: "Contents"
+    fill_in "Status", with: "Status"
+    fill_in "Priority", with: "Priorities"
+    click_on "Create Task"
+    expect(page).to have_text('Task was successfully created.') 
+  end
+
+  scenario "Test task details" do
+    visit tasks_path
+    fill_in "Name", with: "Names"
+    fill_in "Content", with: "Contents"
+    fill_in "Status", with: "Status"
+    fill_in "Priority", with: "Priorities"
+    click_on "Create Task"
+    click_on "Back"
+    click_on "Show"
+    
+    
+    expect(page).to have_content("Contents")
+
+
+  end
+end
